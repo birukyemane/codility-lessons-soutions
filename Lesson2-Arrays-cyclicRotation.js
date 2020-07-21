c93aa8955b2493b2beb138ca3d39089865533579
birukyemane <bir_yem@yahoo.com>
2020-07-21 21:29:23 +0300

    - define cyclicRotation solution

---
+++ Lesson2-Arrays-cyclicRotation.js
@@ -0,0 +1,16 @@
+// Problem : https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+// Author : Biruk Yemane Habteselassie
+
+function solution(A, K) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let splitPoint;
+    if(K % A.length === 0){
+        return A;
+    } else {
+        splitPoint = K % A.length;
+    }
+    
+    let firstPart = A.slice(0,A.length-splitPoint);
+    let lastPart = A.slice(A.length-splitPoint);
+    return lastPart.concat(firstPart);
+}
